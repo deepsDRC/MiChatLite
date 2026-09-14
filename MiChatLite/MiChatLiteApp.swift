7471aa800d290c845d6870abd7a51e96b84db202
deepuramachandran <deepu@apple.com>
2026-09-14 13:22:36 +0530

    Initial MiChatLite iOS project

---
+++ MiChatLite/MiChatLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiChatLiteApp.swift
+//  MiChatLite
+//
+//  Created by Deepu Ramachandran on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MiChatLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiChatLiteTests/MiChatLiteTests.swift
@@ -0,0 +1,19 @@
+//
+//  MiChatLiteTests.swift
+//  MiChatLiteTests
+//
+//  Created by Deepu Ramachandran on 14/09/26.
+//
+
+import Testing
+@testable import MiChatLite
+
+struct MiChatLiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
